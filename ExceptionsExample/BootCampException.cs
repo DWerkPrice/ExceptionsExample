358d59e651f68698ce0338a8bc0053cfd2a34c5f
David Price <dwerkprice@gmail.com>
2020-02-19 15:02:33 -0500

    Add project files.

---
+++ ExceptionsExample/BootCampException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionsExample
+{
+    public class BootCampException : Exception  // 3 constructors should be created
+    {
+        public BootCampException() : base() {}  // 1st constructor
+
+        public BootCampException(string message) : base(message) { }
+
+        public BootCampException(string message , Exception innerException) : base(message , innerException) { }
+
+    }
+}
---
+++ ExceptionsExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExceptionsExample
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            var a = 1;
+            var b = 1;
+
+            try {
+                var x = "ABC";
+                x = null;
+                var len = x.Length;
+            }catch(NullReferenceException ex){
+                var bce = new BootCampException("Boot camp exception" , ex);
+                throw bce;
+            }catch(Exception ex) { 
+                Console.WriteLine(ex.Message);
+            }
+
+            try{
+                var c = a / (b - b);
+            }
+            catch(DivideByZeroException ex){
+                Console.WriteLine(ex.Message);
+            }
+
+
+            
+//            var d = 1 / 0;  visual studio catches this one but not var c = a/(b-b);
+        }
+    }
+}
